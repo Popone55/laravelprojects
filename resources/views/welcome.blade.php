4042b3a6e73b85e21a9de0f61bb6a37573d12f8f
vagrant <vagrant@homestead>
2018-12-04 11:28:14 +0000

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    welcome to Laravel
                 </div>
 
                 <div class="links">
